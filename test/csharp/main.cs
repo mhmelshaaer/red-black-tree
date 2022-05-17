831b76cc8be5cb5d65de6c4d18d7e16cead3f16d
Mouhammed El shaaer <mhmelshaaerrdi@gmail.com>
2022-05-17 12:19:45 +0200

    Add csharp tests:
    - Upgrade the CMake minimum required version to resolve generated wrapper target name conflict.
    - Add the test/csharp folder.
    - Update the ./CMakeLists.txt and test/CMakeLists.txt to reflect the new changes.

---
+++ test/csharp/main.cs
@@ -0,0 +1,13 @@
+﻿public class Test
+{
+	static void Main(string[] args)
+	{
+        int_rbtree T = new int_rbtree();
+        T.insert(3);
+        T.insert(5);
+        T.insert(7);
+        T.print_tree();
+        T.remove(5);
+        T.print_tree();
+    }
+}
